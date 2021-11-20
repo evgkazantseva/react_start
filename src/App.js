b72aef70253131001c81f0e8b9ac52d63e2d53b4
Evgeniia Kazantceva <evg.kazantseva@gmail.com>
2021-11-20 14:27:16 +0500

    chart diagram

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { Bar, Pie } from 'react-chartjs-2';
+
+const data = {
+  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Orange'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [15, 20, 3, 5, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 2,
+    },
+  ],
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className='App'>
+        <h1 className='title'>Pie Chart</h1>
       </header>
+      <Pie data={data} />
     </div>
   );
 }
